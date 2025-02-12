23e14d1a2c8df05a22b928393ef9e54dd9719ca4
NickVendetti <nrvendetti@gmail.com>
2025-02-12 18:53:31 -0500

    connecting to API

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,32 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import fetchPhotos from "./api/flickrApi";
 
 function App() {
+  const [photos, setPhotos] = useState([]);
+
+  useEffect(() => {
+    async function getPhotos() {
+      const results = await fetchPhotos("3d girl"); // Search for 'sunset' photos
+      setPhotos(results);
+    }
+    getPhotos();
+  }, []);
+  
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<h1>Home Page</h1>} />
-        <Route path="/profile" element={<h1>Photographer Profile</h1>}/>
-      </Routes>
-    </Router>
+    <div>
+      <h1>Photo Finder</h1>
+      <ul>
+        {photos.map((photo) =>(
+          <li key={photo.id}>
+            <img src={`https://live.staticflickr.com/${photo.server}/${photo.id}_${photo.secret}_q.jpg`}
+            alt={photo.title}
+            />
+            <p>{photo.title}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
-   
 
 export default App;
