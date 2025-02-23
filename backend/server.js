8604354a9384dcdcfed22b4780ed55319635a5b3
milesangelo <milesvendetti@gmail.com>
2025-02-23 17:00:49 -0500

    updates

--- backend/server.js
+++ backend/server.js
@@ -16,7 +16,13 @@ import authRoutes from "./routes/auth.js";
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors(
+  {
+    origin: "*",
+    methods: "GET,POST,PUT,DELETE",
+    allowedHeaders: "Content-Type,Authorization"
+  }
+));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
