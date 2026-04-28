6861eeb99f3390c9f758023115806ccba336a9c1
NickVendetti <nrvendetti@gmail.com>
2026-04-27 22:50:27 -0400

    chore(api): resolve merge conflict in Program.cs

--- PhotoFinder.Api/PhotoFinderAPI/Program.cs
+++ PhotoFinder.Api/PhotoFinderAPI/Program.cs
@@ -1,38 +1,41 @@
-using PhotoFinderAPI.Middleware;
-using PhotoFinderAPI.Services;
-
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddEndpointsApiExplorer();
+using PhotoFinderAPI.Middleware;                                                                                                                              
+using PhotoFinderAPI.Services;                                                                                                                                
+                                                                                                                                                                
+var builder = WebApplication.CreateBuilder(args);                                                                                                             
+   
+builder.Services.AddEndpointsApiExplorer();                                                                                                                   
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowFrontend", policy =>
-    {
-        policy.WithOrigins("http://localhost:5173").AllowAnyHeader().AllowAnyMethod();
-    });
-});
 
 // Add services to the container.
 builder.Services.AddControllers();
 builder.Services.AddSingleton<IPhotographerService, PhotographerService>();
-// builder.Services.AddScoped<IPhotographerService, RealPhotographerService>();
+// builder.Services.AddScoped<IPhotographerService, RealPhotographerService>();                                                                               
+   
+var corsOrigins =                                                                                                                                             
+    builder.Configuration.GetSection("Cors:AllowedOrigins").Get<string[]>()
+    ?? ["http://localhost:5173"];                                                                                                                             
+                  
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowFrontend", policy =>
+    {                                                                                                                                                         
+        policy.WithOrigins(corsOrigins).AllowAnyHeader().AllowAnyMethod();
+    });                                                                                                                                                       
+});             
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment())                                                                                                                          
 {
-    app.UseSwagger();
+    app.UseSwagger();                                                                                                                                         
     app.UseSwaggerUI();
 }
 
-// Configure the HTTP request pipeline.
 app.UseMiddleware<GlobalExceptionHandlerMiddleware>();
 app.UseMiddleware<RequestLoggingMiddleware>();
-app.UseCors("AllowFrontend");
+app.UseCors("AllowFrontend");                                                                                                                                 
 app.UseHttpsRedirection();
-app.UseAuthorization();
+app.UseAuthorization();                                                                                                                                       
 app.MapControllers();
 
 app.Run();
